8adf1d0efb976ae4847a3904d530db84c209c639
Daniel <50045402+daniel-bejamas@users.noreply.github.com>
2019-10-20 11:14:12 +0200

    Fix xl breakpoint

--- gatsby/src/utils/media.js
+++ gatsby/src/utils/media.js
@@ -7,6 +7,6 @@ const xs = media.lessThan('sm')
 const sm = media.between('sm', 'md')
 const md = media.between('md', 'lg')
 const lg = media.between('lg', 'xl')
-const xl = media.lessThan('xl')
+const xl = media.greaterThan('xl')
 
 export default { xs, sm, md, lg, xl }
