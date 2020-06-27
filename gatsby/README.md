27c67abf23befba07016ee1aef67537a1fd4bc4d
daniel-bejamas <daniel@bejamas.io>
2020-06-27 15:02:53 +0200

    minimal gatsby starter

--- gatsby/README.md
+++
@@ -1,1 +0,0 @@
-# Simple Gatsby starter with styled-components
--- gatsby/src/components/Main/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export default ({ children, className }) => (
-  <main className={className}>{children}</main>
-)
